3b34328d093c8a6fca526cdd4c1f985b1e677986
Autumn <ben.morris0623@gmail.com>
2024-12-11 18:56:32 +0000

    successfully did day01.1

--- day01/main.py
+++ day01/main.py
@@ -1,3 +1,19 @@
 file = open("input.txt")
+list_a = []
+list_b = []
+list_s = []
 for line in file:
-    print(line.rstrip())
+    line = line.rstrip()
+    # split the line into two ints
+    list = line.split()
+    list_a.append(int(list[0]))
+    list_b.append(int(list[1]))
+
+list_a.sort()
+list_b.sort()
+
+solution = 0
+for i in range(len(list_a)):
+    solution = solution + abs(list_b[i] - list_a[i])
+
+print(solution)
